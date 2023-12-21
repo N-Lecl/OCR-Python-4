73448c50fea810084dab61cb9fa9c282f3bbe44e
Nicolas Leclercq <N.Leclercq62@gmail.com>
2023-12-21 22:09:50 +0100

    Déplacements des Displays dans le fichier view

--- controllers/player_controller.py
+++ controllers/player_controller.py
@@ -9,39 +9,28 @@ class PlayerController:
         self.player_view = player_view if player_view else PlayerView()
 
     def add_player_menu(self):
-        print("\n::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
-        print("   _    _             _         _ _               __                              ")
-        print("  /_\  (_) ___  _   _| |_    __| ( _   _ _ __     \ \  ___  _   _  ___ _   _ _ __ ")
-        print(" //_\\ | |/ _ \| | | | __|  / _` || | | | '_ \     \ \/ _ \| | | |/ _ | | | | '__|")
-        print("/  _  \| | (_) | |_| | |_  | (_| || |_| | | | | /\_/ | (_) | |_| |  __| |_| | |   ")
-        print("\_/ \__/ |\___/ \__,_|\__|  \__,_| \__,_|_| |_| \___/ \___/ \__,_|\___|\__,_|_|   ")
-        print("     |__/                                                                         ")
-        print("\n::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
-        first_name = input("\nEntrez le prénom du joueur : ")
-        last_name = input("Entrez le nom de famille du joueur : ")
-        birth_date = input("Entrez la date de naissance du joueur (YYYY-MM-DD) : ")
-        chess_id = input("Entrez l'identifiant national d'échecs du joueur : ")
+        self.player_view.display_add_player_menu()
+        first_name = self.player_view.get_player_data_input("le prénom")
+        last_name = self.player_view.get_player_data_input("le nom de famille")
+        birth_date = self.player_view.get_player_data_input("la date de naissance (YYYY-MM-DD)")
+        chess_id = self.player_view.get_player_data_input("l'identifiant national d'échecs")
 
         new_player = Player(first_name, last_name, birth_date, chess_id)
         self.add_player(new_player)
 
     def add_player(self, player):
+        # Charge les joueurs depuis le fichier JSON
+        self.load_players_from_json('players_infos.json') 
         self.players.append(player)
-        self.save_players_to_json('players_infos.json') 
-        print(f"Joueur ajouté : {player.first_name} {player.last_name}")
+        # Sauvegarde les joueurs après l'ajout
+        self.save_players_to_json('players_infos.json')  
+        self.player_view.display_player_added(player)
 
     def display_all_players(self):
         while True:
-            print("\n::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
-            print("   __ _     _             _              _                                 ")
-            print("  / /(_)___| |_ ___    __| | ___ ___    (_) ___  _   _  ___ _   _ _ __ ___ ")
-            print(" / / | / __| __/ _ \  / _` |/ _ / __|   | |/ _ \| | | |/ _ | | | | '__/ __|")
-            print("/ /__| \__ | ||  __/ | (_| |  __\__ \   | | (_) | |_| |  __| |_| | |  \__ \ ")
-            print("\____|_|___/\__\___|  \__,_|\___|___/  _/ |\___/ \__,_|\___|\__,_|_|  |___/")
-            print("                                      |__/                                 ")
-            print("\n::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
-            print("")
-            self.load_players_from_json('players_infos.json')
+            self.player_view.display_players_list_menu()
+            # Charge les joueurs depuis le fichier JSON
+            self.load_players_from_json('players_infos.json')  
             if not self.players:
                 print("Aucun joueur enregistré.")
             else:
@@ -50,7 +39,7 @@ class PlayerController:
 
             print("\n1. Retour au menu Joueurs")
 
-            choice = input("\nChoisissez une option : ")
+            choice = self.player_view.get_player_input("\nChoisissez une option : ")
             if choice == "1":
                 break
             else:
@@ -67,4 +56,3 @@ class PlayerController:
                 self.players = [Player(**data) for data in player_data]
         except FileNotFoundError:
             print("\nLe fichier JSON n'a pas été trouvé.")
-
